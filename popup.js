127979fd66e007f4c7cba6d843d7681718d63e2e
zhenruyan <1263580126@qq.com>
2017-08-25 22:24:42 +0800

    click

--- popup.js
+++ popup.js
@@ -24,15 +24,29 @@ var loginstatus = new Vue({
   el: '.vroot',
 data: {
     user:{
-      status:1
+      status:0,
+      hison:0
     }
 },
 methods: {
   logout() {
     loginstatus.user.status=0;
+    loginstatus.user.hison=0;
   },
   login(){
     loginstatus.user.status=1;
+  },pushall(){
+    console.log('pushall');
+  },
+  cloneall(){
+      console.log('cloneall');
+  },
+  newhistview(){
+    console.log('hist')
+    loginstatus.user.hison=1;
+  },
+  xhist(){
+    loginstatus.user.hison=0;
   }
 
 }
