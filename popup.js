80173c974f6b8528db11d940ef94cb3d3bb74a38
zhenruyan <1263580126@qq.com>
2017-09-03 17:01:54 +0800

    x

--- popup.js
+++ popup.js
@@ -1,53 +1,36 @@
-chrome.bookmarks.getTree(arr=>{localStorage.setItem('a',JSON.stringify(arr))})
-
-function tijia() {
-  fetch("http://127.0.0.1:3000/",{
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      data: localStorage.getItem('a')
-    })
-  }).then(function(response) {
-    return response;
-  }).catch(
-    err=>{
-      return err
-    }
-  );
+function getuserdata() {
+  let userststus = localStorage.getItem('userdata');
+  let userdata = JSON.parse(userststus);
+  return userdata;
 }
-
-
-var loginstatus = new Vue({
-  el: '.vroot',
-data: {
-    user:{
-      status:0,
-      hison:0
+var root = new Vue({
+  el: '.roms',
+  data: {
+    page: 1,
+    user: {
+      status: 0
     }
-},
-methods: {
-  logout() {
-    loginstatus.user.status=0;
-    loginstatus.user.hison=0;
   },
-  login(){
-    loginstatus.user.status=1;
-  },pushall(){
-    console.log('pushall');
-  },
-  cloneall(){
-      console.log('cloneall');
-  },
-  newhistview(){
-    console.log('hist')
-    loginstatus.user.hison=1;
-  },
-  xhist(){
-    loginstatus.user.hison=0;
+  methods: {
+    users() {
+        root.page = 1;
+      },
+      syncs() {
+        root.page = 2;
+      },
+      tools() {
+        root.page = 3;
+      },
+      msgs() {
+        root.page = 4;
+      },
+      login() {
+        root.user.status = 1;
+      },
+      logout() {
+        root.user.status = 0;
+      }
+
+
   }
-
-}
 });
